ee0f2125cadfbe422f23f5f1827635eaf4fff9f5
Marcel Wiessler <marcel@gaisterhand.de>
2025-09-29 12:31:11 +0200

    add qrcode

--- Scroll Bike Website/src/main.ts
+++ Scroll Bike Website/src/main.ts
@@ -1,1 +1,11 @@
 import("@needle-tools/engine") /* async import of needle engine */;
+import { DeviceUtilities } from "@needle-tools/engine";
+
+document.addEventListener("DOMContentLoaded", () => {
+    if(DeviceUtilities.isMobileDevice()) {
+        const qr_code = document.querySelector('.qr-code');
+        if(qr_code) {
+            qr_code.remove();
+        }
+    }
+})
